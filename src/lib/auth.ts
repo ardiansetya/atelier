f239408d828c66516af213303d5dc22ede8a686d
ardiansetya <ardiansetya129@gmail.com>
2026-01-19 21:35:56 +0700

    setup db

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,24 @@
-import { betterAuth } from 'better-auth'
+import { prisma } from '@/db';
+import { betterAuth } from 'better-auth';
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { tanstackStartCookies } from "better-auth/tanstack-start";
 
 export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql", 
+}),
+
+
   emailAndPassword: {
     enabled: true,
   },
+
+  socialProviders: { 
+    github: { 
+      clientId: process.env.GITHUB_CLIENT_ID as string, 
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string, 
+    }, 
+  }, 
+
+  plugins: [tanstackStartCookies()]
 })
